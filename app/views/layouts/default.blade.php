ab638b1ddbcb89e3bfb2aab364364d9af962cee8
Richard Pett <rich_pett@hotmail.com>
2015-01-25 10:39:07 +0000

    Added gulp

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,9 +4,12 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
 	<title>Larabook</title>
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 
 <body>
+
+	@include('layouts.partials.nav')
 
 	<div class="container">
 		@yield('content')
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-	Welcome to Larabook!
+	<div class="jumbotron">
+        <h1>Welcome to Larabook!</h1>
+        <p>Welcome to the premier place to talk about Laravel with other developers, why don't you sign up to see what all the fuss is about?</p>
+        <p>
+        	<a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up »</a>
+        </p>
+    </div>
 
 @stop
