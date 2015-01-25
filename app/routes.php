46fb0c60723c7b3a3bddb4472d21d40e0db07988
Richard Pett <rich_pett@hotmail.com>
2015-01-25 11:28:34 +0000

    Registration with BDD, created a functional test to sign up with Larabook

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,30 @@
 <?php
 
-Route::get('/', 'PagesController@home');
+/*
+|--------------------------------------------------------------------------
+| Static Pages
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('/', [
+	'as' => 'home',
+	'uses' => 'PagesController@home'
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Registration
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('register', [
+	'as' => 'register_path',
+	'uses' => 'RegistrationController@create'
+]);
+
+Route::post('register', [
+	'as' => 'register_path',
+	'uses' => 'RegistrationController@store'
+]);
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -6,7 +6,9 @@
         <h1>Welcome to Larabook!</h1>
         <p>Welcome to the premier place to talk about Laravel with other developers, why don't you sign up to see what all the fuss is about?</p>
         <p>
-        	<a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up »</a>
+        	
+        	{{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
+
         </p>
     </div>
 
