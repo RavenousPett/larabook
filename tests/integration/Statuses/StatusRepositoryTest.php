ef1a982523afb99873c382f65e638d25999696bb
Richard Pett <rich_pett@hotmail.com>
2015-05-24 17:59:28 +0100

    Finished Integration Testing Repositories

--- tests/integration/Statuses/StatusRepositoryTest.php
+++ tests/integration/Statuses/StatusRepositoryTest.php
@@ -25,11 +25,13 @@ class StatusRepositoryTest extends \Codeception\TestCase\Test
 
         // And statuses for both of them
         TestDummy::times(2)->create('Larabook\Statuses\Status', [
-            'user_id' => $users[0]->id
+            'user_id' => $users[0]->id,
+            'body' => 'My Status'
         ]);
 
         TestDummy::times(2)->create('Larabook\Statuses\Status', [
-            'user_id' => $users[1]->id
+            'user_id' => $users[1]->id,
+            'body' => 'His status'
         ]);
 
         // When I fetch statuses for one user
@@ -37,6 +39,33 @@ class StatusRepositoryTest extends \Codeception\TestCase\Test
 
         // Then I should recieve only the relevant ones
         $this->assertCount(2, $statusesForUser);
+        $this->assertEquals('My Status', $statusesForUser[0]->body);
+        $this->assertEquals('My Status', $statusesForUser[1]->body);        
+    }
+    
+    /** @test */
+    public function it_saves_a_status_for_a_user()
+    {
+        // Given I have an unsaved status
+        $status = TestDummy::create('Larabook\Statuses\Status', [
+            'user_id' => null,
+            'body' => 'My Status'
+        ]);
+
+        // And an existing user
+        $user = TestDummy::create('Larabook\Users\User');
+
+        // When I try to persist this status
+        $savedStaus = $this->repo->save($status, $user->id);
+
+        // Then it should be saved
+        $this->tester->seeRecord('statuses', [
+            'body' => 'My Status',
+        ]);
+
+        // And the status should have the correct user_id
+        $this->assertEquals($user->id, $savedStaus->user_id);
+
     }
 
 }
